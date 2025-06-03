2116450d2ceae438527aa7a7b099962c0d0c265a
Angel Echavarria <echavarriasam@gmail.com>
2025-06-03 01:14:11 +0000

    coffee example

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from smolagents import CodeAgent,DuckDuckGoSearchTool, HfApiModel,load_tool,tool
 import datetime
+import random
 import requests
 import pytz
 import yaml
@@ -11,7 +12,7 @@ from Gradio_UI import GradioUI
 @tool
 def my_custom_tool(arg1:str, arg2:int)-> str: #it's important to specify the return type
     #Keep this format for the description / args / args description but feel free to modify the tool
-    """A tool that does nothing yet 
+    """A tool that does nothing yet
     Args:
         arg1: the first argument
         arg2: the second argument
@@ -26,6 +27,12 @@ def multiply(arg1:int, arg2:int)-> str:
         arg2: the second number
     """
     return str(arg1 * arg2)
+
+@tool
+def coffe()-> str:
+    """A tool that tells how is the temperature of the coffee"""
+    coffee_states = ["cold","warm","hot", "burning"]
+    return random.choice(coffee_states)
 
 @tool
 def get_current_time_in_timezone(timezone: str) -> str:
@@ -46,7 +53,7 @@ def get_current_time_in_timezone(timezone: str) -> str:
 final_answer = FinalAnswerTool()
 
 # If the agent does not answer, the model is overloaded, please use another model or the following Hugging Face Endpoint that also contains qwen2.5 coder:
-# model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
+# model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud'
 
 model = HfApiModel(
 max_tokens=2096,
@@ -61,7 +68,7 @@ image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_co
 
 with open("prompts.yaml", 'r') as stream:
     prompt_templates = yaml.safe_load(stream)
-    
+
 agent = CodeAgent(
     model=model,
     tools=[final_answer], ## add your tools here (don't remove final answer)
